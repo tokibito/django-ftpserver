6921e0d7e8e393e2458c324d7091ef65b86af57b
Shinya Okano <shinya.okano@beproud.jp>
2014-03-26 05:34:19 +0900

    modify requirements for doc

--- src/django_ftpserver/admin.py
+++ src/django_ftpserver/admin.py
@@ -1,16 +1,20 @@
 from django.contrib import admin
 
-from .models import FTPUserGroup, FTPUserAccount
+from . import models
 
 
 class FTPUserGroupAdmin(admin.ModelAdmin):
+    """Admin class for FTPUserGroup
+    """
     list_display = ('name', 'permission')
     search_fields = ('name', 'permission')
 
 
 class FTPUserAccountAdmin(admin.ModelAdmin):
+    """Admin class for FTPUserAccountAdmin
+    """
     list_display = ('user', 'group', 'last_login')
     search_fields = ('user', 'group', 'last_login')
 
-admin.site.register(FTPUserGroup, FTPUserGroupAdmin)
-admin.site.register(FTPUserAccount, FTPUserAccountAdmin)
+admin.site.register(models.FTPUserGroup, FTPUserGroupAdmin)
+admin.site.register(models.FTPUserAccount, FTPUserAccountAdmin)
