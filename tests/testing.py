0c1b7add527475c980430e3244cf7d242a387981
Shinya Okano <shinya.okano@beproud.jp>
2014-11-01 19:05:31 +0900

    modify testing utility for Django 1.7

--- tests/testing.py
+++ tests/testing.py
@@ -13,10 +13,20 @@ def setup_django():
                 'django.contrib.auth',
                 'django.contrib.contenttypes',
                 'django_ftpserver',
-            )
+            ),
+            MIDDLEWARE_CLASSES=(),
         )
-    from django.db.models.loading import cache as model_cache
-    if not model_cache.loaded:
-        model_cache._populate()
-        from django.core.management import call_command
-        call_command('syncdb', interactive=False)
+    from django import VERSION as version
+    # In Django 1.7 or later, using "django.apps" module.
+    if version[0] == 1 and version[1] >= 7:
+        from django.apps import apps
+        if not apps.ready:
+            apps.populate(settings.INSTALLED_APPS)
+            from django.core.management import call_command
+            call_command('syncdb', interactive=False)
+    else:
+        from django.db.models.loading import cache as model_cache
+        if not model_cache.loaded:
+            model_cache._populate()
+            from django.core.management import call_command
+            call_command('syncdb', interactive=False)
