5e93f46a12268c3b55269d205669c72cd3682c35
Shinya Okano <tokibito@gmail.com>
2015-10-14 00:51:54 +0900

    test with python 2.6.

--- tests/test_compat.py
+++ tests/test_compat.py
@@ -4,4 +4,4 @@ from unittest import TestCase
 class StringTypeTest(TestCase):
     def test(self):
         from django_ftpserver.compat import string_type
-        self.assertIsNotNone(string_type)
+        self.assertTrue(string_type)
