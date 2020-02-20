688c9d50668b8f8f1017f0c994a2950ad7d04ae6
Shinya Okano <shinya.okano@open-c.jp>
2020-02-20 23:35:26 +0900

    modify docs config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,8 +33,11 @@ def setup_django():
                 }
             },
             INSTALLED_APPS=(
+                'django.contrib.admin',
                 'django.contrib.auth',
                 'django.contrib.contenttypes',
+                'django.contrib.sessions',
+                'django.contrib.messages',
                 'django_ftpserver',
             )
         )
