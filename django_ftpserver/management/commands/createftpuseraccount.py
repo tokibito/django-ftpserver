e4d225dfceed8b0d7b5e61f153cad955be68d2dc
Maciej Parol <m.parol@rynekpierwotny.pl>
2021-02-02 15:03:03 +0100

    Some fixes in commands

--- django_ftpserver/management/commands/createftpuseraccount.py
+++ django_ftpserver/management/commands/createftpuseraccount.py
@@ -1,48 +1,50 @@
-import sys
-
-from django.core.management.base import BaseCommand, CommandError
 from django.contrib.auth import get_user_model
+from django.core.management.base import BaseCommand, CommandError
 
 from django_ftpserver import models
 from django_ftpserver.compat import get_username_field
+
+
+def non_existent_user_account(username):
+    if models.FTPUserAccount.objects.filter(user__username=username).exists():
+        raise CommandError(f"FTP user group {username} is already exists.")
+    else:
+        return username
 
 
 class Command(BaseCommand):
     help = "Create FTP user account"
 
     def add_arguments(self, parser):
-        parser.add_argument('username')
-        parser.add_argument('group')
-        parser.add_argument('home_dir', nargs='?')
+        parser.add_argument("username", type=non_existent_user_account)
+        parser.add_argument("group")
 
     def handle(self, *args, **options):
-        username = options.get('username')
-        group_name = options.get('group')
-        home_dir = options.get('home_dir')
-
-        if models.FTPUserAccount.objects.filter(
-                user__username=username).exists():
-            raise CommandError(
-                'FTP user account "{username}" is already exists.'.format(
-                    username=username))
+        username = options.get("username")
+        group_name = options.get("group")
 
         User = get_user_model()
         try:
             user = User.objects.get(**{get_username_field(): username})
         except User.DoesNotExist:
             raise CommandError(
-                'User "{username}" is not exists.'.format(username=username))
+                'User "{username}" is not exists.'.format(username=username)
+            )
 
         try:
             group = models.FTPUserGroup.objects.get(name=group_name)
         except models.FTPUserGroup.DoesNotExist:
             raise CommandError(
-                'FTP user group "{name}" is not exists.'.format(
-                    name=group_name))
-
+                'FTP user group "{name}" is not exists.'.format(name=group_name)
+            )
+        main_directory = options.get("main_directory")
+        home_dir = f"{f'/{main_directory}' if main_directory else ''}/{username}"
         account = models.FTPUserAccount.objects.create(
-            user=user, group=group, home_dir=home_dir)
+            user=user, group=group, home_dir=home_dir
+        )
 
-        sys.stdout.write(
+        self.stdout.write(
             'FTP user account pk={pk}, "{username}" was created.\n'.format(
-                pk=account.pk, username=username))
+                pk=account.pk, username=username
+            )
+        )
--- django_ftpserver/management/commands/createftpusergroup.py
+++ django_ftpserver/management/commands/createftpusergroup.py
@@ -1,34 +1,44 @@
-import sys
-
 from django.core.management.base import BaseCommand, CommandError
 
 from django_ftpserver import models
 
 
+def non_existent_user_group(name):
+    if models.FTPUserGroup.objects.filter(name=name).exists():
+        raise CommandError(f"FTP user group {name} is already exists.")
+    else:
+        return name
+
+
 class Command(BaseCommand):
     help = "Create FTP user group"
 
-    def add_arguments(self, parser):
-        parser.add_argument('name')
-        parser.add_argument('home_dir', nargs='?')
+    def add_arguments(self, parser) -> None:
+        parser.add_argument(
+            "name",
+            type=non_existent_user_group,
+        )
 
         parser.add_argument(
-            '--permission', action='store', dest='permission',
-            help="permission for home directory.")
+            "--main_directory",
+            action="store",
+            dest="mainly_directory",
+            help="Mainly directory.",
+        )
+        parser.add_argument(
+            "--permission",
+            action="store",
+            dest="permission",
+            help="permission for home directory.",
+        )
 
     def handle(self, *args, **options):
-        name = options.get('name')
-        home_dir = options.get('home_dir')
-
-        if models.FTPUserGroup.objects.filter(name=name).exists():
-            raise CommandError(
-                "FTP user group {name} is already exists.".format(name=name))
-
+        name = options.get("name")
+        main_directory = options.get("main_directory")
+        home_dir = f"{f'/{main_directory}' if main_directory else ''}/{name}"
         group = models.FTPUserGroup(name=name, home_dir=home_dir)
-        if options['permission']:
-            group.permission = options['permission']
+        if options["permission"]:
+            group.permission = options["permission"]
         group.save()
 
-        sys.stdout.write(
-            "FTP user group pk={pk}, {name} was created.\n".format(
-                pk=group.pk, name=name))
+        self.stdout.write(f"FTP user group pk={group.pk}, {name} was created.\n")
