73099c1e1f616bbfb929268287d74a4ce7991a71
Shinya Okano <shinya.okano@open-c.jp>
2026-01-17 19:45:13 +0900

    change sphinx theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -58,6 +58,7 @@ setup_django()
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.todo',
+    'sphinx_rtd_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -129,7 +130,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
